04830aeac8be6d011362178aba70f24cdcfb0766
Marco <markoka1@outlook.it>
2020-12-01 23:00:07 +0100

    implementazione vue js. Allocazione dinamica delle immagini nella sezione "works". Slider citazioni nella sezione "citazioni", brand allocati dinamicamente

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            indice_citazioni: 0,
+            citazioni: [
+                {
+                    autore: "Steve Jobs - Apple",
+                    citazione: " 'That's been one of my mantras - focus and simplicity. Simple can be harder than complex; you have to work hard to get your thinking clean to make it simple.' "
+                },
+                {
+                    autore: "Steve Jobs - Apple",
+                    citazione: " 'La cosa più preziosa che puoi fare è un errore: non imparerai nulla dall'essere perfetto.' "
+                }
+            ],
+            brands: ["images/logo_demo_1.png", "images/logo_demo4.png", "images/Vector_Smart_Object-1.png", "images/Vector_Smart_Object-2.png", "images/Vector_Smart_Object-3.png"],
+            works: ["images/portfolio1_featured_item-1200x960.jpg","images/portfolio5_featured_item-1200x960.jpg","images/portfolio4_featured_item-1200x960.jpg","images/portfolio3_featured_item-1200x960.jpg","images/portfolio2_featured_item-1200x960.jpg","images/portfolio6_featured_item-1200x960.jpg"]
+        },
+        methods: {
+            next_image() {
+                // incremento l'indice della citazione corrente
+                this.indice_citazioni += 1;
+                // verifico l'indice è uscito dal range delle posizioni dell'array
+                // ossia se ho superato il limite superiore
+                if(this.indice_citazioni > this.citazioni.length - 1) {
+                    this.indice_citazioni = 0;
+                }
+            },
+            prev_image() {
+                // decremento l'indice della citazione corrente
+                this.indice_citazioni -= 1;
+                // verifico se l'indice è uscito dal range delle posizioni dell'array
+                // ossia se ho superato il limite inferiore
+                if(this.indice_citazioni < 0) {
+                    this.indice_citazioni = this.citazioni.length - 1;
+                }
+            },
+            change(index){
+                this.indice_citazioni = index;
+            }
+        }
+    }
+);
